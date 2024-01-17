2e26f72b290e217f4d582d04153a277c88b7e2b0
yaroslavdrapei <yarikdrapey@gmail.com>
2024-01-17 12:05:46 +0200

    added gitignore; created tokenization

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        var tokenizer = new Tokenizer();
+        var tokens = tokenizer.Tokenize(Console.ReadLine());
     }
 }
---
+++ Tokenization.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+using TestProjectApp;
+
+namespace Assignment1;
+
+public class Tokenizer
+{
+    public CustomQueue<string> Tokenize(string input)
+    {
+        string digits = "";
+        CustomQueue<string> queue = new();
+        foreach (var chr in input)
+        {
+            if (char.IsDigit(chr)) { digits += chr; }
+            else if (chr == ' ')
+            {
+                if (digits == "") continue;
+                queue.Enqueue(digits);
+                digits = "";
+            }
+            else
+            {
+                if (digits != "")
+                {
+                    queue.Enqueue(digits);
+                    digits = "";  
+                };
+                queue.Enqueue(chr.ToString());
+            }
+        }
+        
+        if (digits != "") queue.Enqueue(digits);
+
+        return queue;
+    }
+}
